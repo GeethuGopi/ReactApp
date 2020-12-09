063efbb55bf2f2a6b0f5fab92b632efcc0c9cbf8
Geethu Gopi <geethugopi33@gmail.com>
2020-12-10 12:06:33 +1300

    added conditional nav items

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return ( 
-        <footer className="text-center">
-            The footer
+        <footer className="absolute bottom-0 w-full text-xs text-gray-700 text-center bg-gray-400 px-4 py-2">
+            My React App &copy; All Rights Reserved 2020
         </footer>
-    );
+     );
 }
  
 export default Footer;
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faBars} from "@fortawesome/free-solid-svg-icons"
 const Navigation = () => {
+    const [displayMenu,setDisplayMenu] = useState(false)
+    let menuItems; 
+    if (displayMenu){
+        menuItems = <div className= "bg-white fixed top-0 left-0 w-3/5 shadow-md h-full z-50">The menu items</div>}
     return (
         <nav>
-            <span className="text-xl cursor-pointer text-lg">
-                <FontAwesomeIcon icon={faBars} />
+            <span className="text-xl cursor-pointer ">
+                <FontAwesomeIcon icon={faBars} className="text-lg" 
+                    onClick={()=>setDisplayMenu(!displayMenu)}/>
             </span>
+            {menuItems}
         </nav> 
         );
 }
